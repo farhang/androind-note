c2083932f9acfc32f14bfb69c2f8b3b4a6a83b04
Farhang Darzi <F.Darzi@NSedna.com>
2021-08-01 19:43:14 +0430

    fix: event listerner.

--- src/App.js
+++ src/App.js
@@ -6,15 +6,41 @@ import {
   Route,
 } from "react-router-dom";
 import LockScreen from "./lockScreen/LockScreen";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Home from "./home/Home";
 import Header from "./common/header/Header";
+import eventBus from "./common/Eventbus";
 
 function App() {
+    let visibility = 'visible';
+    eventBus.on("touchStatus", (event) => {
+        if(event.status === 'move' && visibility === 'visible') {
+            console.log('start')
+            visibility = 'hidden';
+            setStyles({
+                visibility: visibility,
+                opacity: 0,
+            })
+        } else  if (event.status === 'end'){
+            console.log('move')
+            visibility = 'visible';
+            setStyles({
+                visibility: visibility,
+                opacity: 1,
+            })
+        }
+    } );
+
+    const [styles, setStyles] = useState({});
+
+    useEffect(() => {
+
+    }, []);
   return (
       <Router>
-          <Header>
-          </Header>
+          <div className={"header"} style={styles}>
+          <Header />
+          </div>
           <Switch>
             <Route path="/">
               <LockScreen />
--- src/lockScreen/LockScreen.js
+++ src/lockScreen/LockScreen.js
@@ -1,15 +1,82 @@
 import './LockScreen.css';
+import {useState, useEffect} from "react";
+import eventBus from "../common/Eventbus";
 
 function LockScreen() {
-    const currentDate = new Date();
-    const currentHour  = ('0' + currentDate.getHours()).substr(-2);
-    const currentMinute = ('0' + currentDate.getMinutes()).slice(-2);
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [currentHour, setCurrentHour] = useState('');
+    const [currentMinute, setCurrentMinute] = useState('');
     const dateOptions = { weekday: 'short', day: 'numeric', month: 'long' };
+    let [x1, x2, y1, y2] = [0,0,0,0];
+    let touch = document.querySelector('#touch');
+
+    useEffect(() => {
+        handleDate();
+        setInterval(handleDate, 1000);
+        handleTouch();
+    }, []);
+
+    const handleTouch = () => {
+        const body = document.querySelector('body');
+        touch = document.querySelector('#touch');
+        body.addEventListener('touchstart', handleGestureStart, false);
+        body.addEventListener('touchmove', handleGestureMove, {passive: true});
+        body.addEventListener('touchend', handleGestureEnd, false);
+    }
+
+    const handleGestureStart =  (event) => {
+        touch.innerHTML = 'start';
+        x1 =  event.touches[0].clientX;
+        y1 =  event.touches[0].clientY;
+        console.log('start', event.touches[0].clientX)
+        eventBus.dispatch("touchStatus", { status: 'start'})
+    }
+
+    const handleGestureMove =  (event) => {
+        touch.innerHTML = 'move';
+        x2 =  event.touches[0].clientX;
+        y2 =  event.touches[0].clientY;
+        eventBus.dispatch("DistanceCalculated", { value: calculateDistance(x1, y1, x2, y2)})
+        eventBus.dispatch("touchStatus", { status: 'move'})
+    }
+
+    const calculateDistance = (cx1, cy1, cx2, cy2) => {
+        const a = cx1 - cx2;
+        const b = cy1 - cy2;
+        return Math.sqrt( a * a + b * b );
+    }
+
+    const handleGestureEnd =  (event) => {
+        eventBus.dispatch("touchStatus", { status: 'end'})
+        touch.innerHTML = 'end';
+        console.log('end', event);
+    }
+
+
+    const handleDate = () => {
+        setCurrentDate(new Date());
+        setCurrentHour(('0' + currentDate.getHours().toString()).slice(-2));
+        setCurrentMinute(('0' + currentDate.getMinutes().toString()).slice(-2));
+    }
+
     return (
-        <section>
+        <section id="lock-screen">
             <div className="time font-thin">{currentHour}:{currentMinute}</div>
             <div className="date">{currentDate.toLocaleDateString("en-US", dateOptions)}</div>
-        </section>)
+
+            <footer className={"footer"}>
+                <div className={"footer-header font-regular"}>
+                    <span>Swipe to open</span>
+                </div>
+                <div className={"footer-inner"}>
+                    <span className="material-icons footer-inner__item">photo_camera</span>
+                    <span className="footer-inner__item"><span className={"footer-inner__item--phone material-icons"}>call</span></span>
+                </div>
+            </footer>
+        </section>
+
+    )
 }
+
 
 export default LockScreen;
